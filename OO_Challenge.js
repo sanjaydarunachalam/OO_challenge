d5ab374c2ecf2b068910cc5bc8b98e1be0d958a4
Sanjay Arunachalam <sanjaydarunachalam@gmail.com>
2022-04-04 21:57:33 -0400

    Add files via upload

---
+++ OO_Challenge.js
@@ -0,0 +1,51 @@
+class Vehicle {
+    constructor (make, model, year){
+        this.make= make;
+        this.model= model;
+        this.year= year;
+    }
+    
+    honk (){
+        return "Beep.";
+    }
+
+    toString(make, model, year){
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}    
+
+class Car extends Vehicle {
+    constructor (make, model, year) {
+        super (make, model, year);
+        this.numWheels= 4;
+    }
+
+    revEngine (){
+        return "VROOM!!!";
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year){
+        super (make, model, year);
+        this.numWheels = 2;
+    }
+}
+
+class Garage {
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle){
+        if(!(newVehicle instanceof Vehicle)){
+            return "Only vehicles are allowed in here!";
+        }
+        if(this.vehicles.length >= this.capacity){
+            return "Sorry, we’re full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
